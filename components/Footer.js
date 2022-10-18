59f009a31eef32e776285964d05dcd589cb8c0e9
Haneem <kmhaneem00@gmail.com>
2022-10-18 13:01:24 +0530

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>About</h5>
+            <p>Sri Lanka</p>
+            <p>India</p>
+            <p>Pakistan</p>
+            <p>Nepal</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+            <p>Sri Lanka</p>
+            <p>India</p>
+            <p>Pakistan</p>
+            <p>Nepal</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+            <p>Sri Lanka</p>
+            <p>India</p>
+            <p>Pakistan</p>
+            <p>Nepal</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>About</h5>
+            <p>Sri Lanka</p>
+            <p>India</p>
+            <p>Pakistan</p>
+            <p>Nepal</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Footer
